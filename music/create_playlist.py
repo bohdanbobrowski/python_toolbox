b78ca7badc039f5ac313b5f5d49929a04a285275
Bohdan Bobrowski <bohdanbobrowski@gmail.com>
2025-05-04 11:53:55 +0200

    list_extensions

--- music/create_playlist.py
+++ music/create_playlist.py
@@ -11,8 +11,6 @@ MUSIC_EXTENSIONS = [
     "mtm",
     "opus",
     "pdf",
-    "png",
-    "prg",
     "s3m",
     "sf2",
     "sng",
@@ -38,29 +36,34 @@ IGNORED_EXTENSIONS = [
     "m3u",
     "mobi",
     "nfo",
+    "png",
     "prg",
     "txt",
     "ult",
 ]
 
 
-def list_extensions(path: str) -> list[str]:
+def create_playlist(path: str, recursive: bool):
+    pass
+
+
+def list_extensions(path: str, recursive: bool) -> list[str]:
     """This function crawls given path recursely and creates unique list of extensions."""
     print(f"Crawling path '{path}' files...")
 
-    def _list_extensions(p: str) -> list[str]:
+    def _list_extensions(p: str, r: bool) -> list[str]:
         _extensions = []
         dir_contents = os.listdir(p)
         for element in dir_contents:
-            if os.path.isdir(os.path.join(p, element)):
-                _extensions += _list_extensions(os.path.join(p, element))
-            else:
+            if r and os.path.isdir(os.path.join(p, element)):
+                _extensions += _list_extensions(os.path.join(p, element), r)
+            elif os.path.isfile(os.path.join(p, element)):
                 ex = element.split(".")[-1].lower()
                 if ex not in IGNORED_EXTENSIONS:
                     _extensions.append(ex)
         return _extensions
 
-    _list_extensions = set(_list_extensions(path))
+    _list_extensions = set(_list_extensions(path, recursive))
     return sorted(_list_extensions)
 
 
@@ -77,6 +80,12 @@ def main():
         help="This function crawls given path recursely and creates unique list of extensions.",
     )
     parser.add_argument(
+        "-r",
+        "--recursive",
+        action="store_true",
+        help="Recursive.",
+    )
+    parser.add_argument(
         "-o",
         "--overwrite",
         action="store_true",
@@ -85,12 +94,14 @@ def main():
     parser.parse_args()
     args = parser.parse_args()
     if args.list_extensions:
-        extensions = list_extensions(args.path)
+        extensions = list_extensions(args.path, args.recursive)
         print(f"List of extensions in '{args.path}':")
         print("MUSIC_EXTENSIONS = [")
         for _ex in extensions:
             print(f'    "{_ex}",')
         print("]")
+    else:
+        pass
 
 
 if __name__ == "__main__":
