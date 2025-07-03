cdd20c0f1e8a7cd8d7a82431923ee5df7181697f
Bohdan Bobrowski <bohdanbobrowski@gmail.com>
2025-07-03 22:05:26 +0200

    exiftool_csv and extended documentation

--- README.md
+++ README.md
@@ -3,8 +3,56 @@ This is my python toolbox.
 
 ## Installation
 
+    pip install git+https://github.com/bohdanbobrowski/python_toolbox
+
+#### Local (dev) environment
+
     pip install -e .
 
-# Running
+## Included scripts
 
-    :-)
+### Music
+
+#### [create_playlist](music/create_playlist.py)
+    usage: create_playlist [-h] [-l] [-r] [-o] path
+
+    Create playlist - python script which creates a playlists, in subfolders
+
+    positional arguments:
+      path                  Target path
+    
+    options:
+      -h, --help            show this help message and exit
+      -l, --list-extensions
+                            This function crawls given path recursely and creates unique list of extensions.
+      -r, --recursive       Recursive.
+      -o, --overwrite       Always overwrite existing playlist.
+
+### Pictures
+
+#### [exiftool_csv](pictures/exiftool_csv.py)
+    usage: exiftool_csv [-h] [-d] [-c] [-s]
+
+    Exiftool CSV - apply csv values to jpg files using exiftool.
+    
+    options:
+      -h, --help        show this help message and exit
+      -d, --dry-run     Dry run.
+      -c, --create-csv  Create csv template for given folder.
+      -s, --save        Save csv.
+
+#### [move_not_starred](pictures/move_not_starred.py)
+    usage: move_not_starred [-h] [-d] [-p PICTURE_EXTENSION] [-r RAW_EXTENSION] [-t TARGET]
+    
+    Move raw files for jpegs that are not starred to subfolder. Requires exiftool.
+    
+    options:
+      -h, --help            show this help message and exit
+      -d, --dry-run         Dry run.
+      -p PICTURE_EXTENSION, --picture_extension PICTURE_EXTENSION
+                            Picture file extension (by default JPG).
+      -r RAW_EXTENSION, --raw_extension RAW_EXTENSION
+                            RAW file extension (by default DNG).
+      -t TARGET, --target TARGET
+                            Target path (by default creates sub folder with the same name as current one).
+
---
+++ pictures/exiftool_csv.py
@@ -0,0 +1,117 @@
+import argparse
+import os
+import subprocess
+import sys
+from fnmatch import filter
+
+
+def exiftool_csv_save(save: bool = True):
+    if not save:
+        print("DRY RUN!")
+    data = {}
+    with open("exiftool.csv") as f:
+        for line in f:
+            line = line.split(",")
+            if not data:
+                for key in line:
+                    data[key.strip()] = []
+            else:
+                cnt = 0
+                for key in data.keys():
+                    data[key].append(line[cnt].strip())
+                    cnt += 1
+    if data.get("SourceFile"):
+        cnt = 0
+        for source_file in data.get("SourceFile"):
+            if os.path.isfile(source_file):
+                command = ["exiftool"]
+                for key in data.keys():
+                    try:
+                        if key not in ["", "SourceFile"] and data[key][cnt]:
+                            # value = f"\"{data[key][cnt]}\""
+                            value = data[key][cnt]
+                            if key.find("Serial") < 0:
+                                try:
+                                    value = int(data[key][cnt])
+                                except ValueError:
+                                    pass
+                            if value:
+                                command.append(f"-{key}={value}")
+                    except IndexError:
+                        pass
+                command.append(source_file)
+                print(" ".join(command))
+                if save:
+                    subprocess.run(command)
+            cnt += 1
+
+
+def _get_files():
+    files = os.listdir()
+    sorted(files)
+    return filter(files, "*.[Jj][Pp][Gg]")
+
+
+def exiftool_csv_create():
+    columns = [
+        "SourceFile",
+        "Artist",
+        "Title",
+        "Make",
+        "Model",
+        "SerialNumber",
+        "Lens",
+        "LensMake",
+        "LensModel",
+        "LensSerialNumber",
+        "FocalLength",
+        "ISO",
+    ]
+    file_content = ",".join(columns) + "\n"
+    if not os.path.isfile("exiftool.csv"):
+        for image_file in _get_files():
+            empty_columns = "," * (len(columns) - 1)
+            file_content += f"{image_file}{empty_columns}\n"
+        print("blank exiftool.csv created:")
+        print(file_content)
+        with open("exiftool.csv", "w") as f:
+            f.write(file_content)
+            print("...and saved.")
+    else:
+        print("exiftool.csv exists!")
+
+
+def main():
+    parser = argparse.ArgumentParser(
+        prog="exiftool_csv",
+        description="Exiftool CSV - apply csv values to jpg files using exiftool.",
+    )
+    parser.add_argument(
+        "-d",
+        "--dry-run",
+        action="store_true",
+        help="Dry run.",
+    )
+    parser.add_argument(
+        "-c",
+        "--create-csv",
+        action="store_true",
+        help="Create csv template for given folder.",
+    )
+    parser.add_argument(
+        "-s",
+        "--save",
+        action="store_true",
+        help="Save csv.",
+    )
+    parser.parse_args()
+    args = parser.parse_args()
+    if len(sys.argv) == 1:
+        parser.print_help(sys.stderr)
+        sys.exit(1)
+    if args.dry_run:
+        exiftool_csv_save(save=False)
+    elif args.create_csv:
+        exiftool_csv_create()
+    elif args.save:
+        exiftool_csv_save()
